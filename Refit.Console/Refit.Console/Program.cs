faa17f4bba72bd0e1661566d0f4ab88f6497e050
berdikulov-571 <mygithub571@gmail.com>
2023-12-11 12:36:02 +0500

    Добавьте файлы проекта.

---
+++ Refit.Console/Refit.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Refit.Console
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var userApi = RestService.For<IUserAPI>("https://localhost:7065/api");
+            var users = await userApi.GetUsers();
+
+            foreach(var i in users)
+            {
+                System.Console.WriteLine($"{i.UserId}) {i.FirstName} {i.LastName}");
+            }
+        }
+    }
+    public interface IUserAPI
+    {
+        [Get("/User/GetAll")]
+        Task<List<User>> GetUsers();
+    }
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
